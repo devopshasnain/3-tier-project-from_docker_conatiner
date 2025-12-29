79f7bcb527fb836dd6556f3f019b3551b009bfec
Ubuntu <azureuser@myserver.zqpcdvwa2sielmz0yxmc0gcewh.rx.internal.cloudapp.net>
2025-12-29 09:27:55 +0000

    production ready

--- api/app.js
+++ api/app.js
@@ -15,6 +15,9 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
 
 // Routes
 app.use('/api/auth', authRoutes);
